0e2e29e19a24468ef31f40de716bf6480af381fd
PalakKapila72 <24BCS10349@cuchd.in>
2026-02-05 14:22:38 +0530

    Add files via upload

---
+++ Experiment-4/cutoff.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int K, N;
+    cin >> K >> N;
+
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
+    for (int i = 0; i < N; i++) {
+        int score;
+        cin >> score;
+
+        if (minHeap.size() < K) {
+            minHeap.push(score);
+            if (minHeap.size() < K)
+                cout << -1 << endl;
+            else
+                cout << minHeap.top() << endl;
+        }
+        else {
+            if (score > minHeap.top()) {
+                minHeap.pop();
+                minHeap.push(score);
+            }
+            cout << minHeap.top() << endl;
+        }
+    }
+
+    return 0;
+}
